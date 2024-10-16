59717618d23816c0336f8507049c69e2010bd782
William McQuaid <whmcquaid@gmail.com>
2024-10-16 16:21:37 -0700

    Added a check for virtualbox in create VM script and bridge connection if using virtualbox.

--- create-webserver.sh
+++ create-webserver.sh
@@ -1,8 +1,17 @@
 # A VM webserver should exist
+
+
 if (multipass info webserver)
 then
   echo "The VM webserver already exists"
 else
   echo "Creating the VM webserver"
-  multipass launch --name webserver
+  if [ $(multipass get local.driver) != 'virtualbox' ]
+    then 
+      echo "Starting Webserver"
+      multipass launch --name webserver
+    else
+      echo "Starting webserver with bridged connection"
+      multipass launch --name webserver --bridged
+  fi  
 fi
